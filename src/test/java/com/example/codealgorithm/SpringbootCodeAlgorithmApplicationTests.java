8e5848e6b55d2673bdf7bf43396bdbd44d4cb43a
luoxuan <luoxuan@example.com>
2026-07-04 20:16:18 +0800

    Initial commit: Spring Boot project for code algorithm

---
+++ src/test/java/com/example/codealgorithm/SpringbootCodeAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codealgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCodeAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
