ccdef5e0dc64268a9fd03f1b89c742a03969c0d5
luoxuan <luoxuan@example.com>
2026-07-05 19:07:29 +0800

    移动零

---
+++ src/test/java/com/example/codealgorithm/MoveZero283.java
@@ -0,0 +1,45 @@
+package com.example.codealgorithm;
+
+import java.util.ArrayList;
+
+public class MoveZero283 {
+    public static void main(String[] args) {
+        int[]nums=new int[] { 0, 1, 0, 3, 12 };
+        new MoveZero283().moveZeroes1(nums);
+        System.out.println("nums = " + java.util.Arrays.toString(nums));
+    }
+    // 用空间换时间
+    public void moveZeroes(int[] nums) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i : nums) {
+            if (i != 0) {
+                arrayList.add(i);
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i < arrayList.size()) {
+                nums[i] = arrayList.get(i);
+            } else {
+                nums[i] = 0;
+            }
+        }
+    }
+    // 函数功能跟moveZeroes相同，但是用两个指针以及位置交换实现
+    public void moveZeroes1(int[] nums) {
+        int left = 0;
+        int right = 0;
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                nums[left] = nums[right];
+                left++;
+            }
+            right++;
+        }
+        while (left < nums.length) {
+            nums[left] = 0;
+            left++;
+        }
+    }
+
+}
