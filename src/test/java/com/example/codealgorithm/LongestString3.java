c87098d8eb87c22c5a6e646a079f450e267f3c0f
luoxuan <luoxuan@example.com>
2026-07-06 19:51:26 +0800

    滑动窗口

---
+++ src/test/java/com/example/codealgorithm/LongestString3.java
@@ -0,0 +1,27 @@
+package com.example.codealgorithm;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class LongestString3 {
+    public static void main(String[] args) {
+        LongestString3 foo = new LongestString3();
+        System.out.println(foo.lengthOfLongestSubstring("pwwkew"));
+    }
+
+    // 滑动窗口的思路，需要确定的是：left第二次移动到相同字符串位置，right只是继续移动+1
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int maxLength = 0;
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            if (map.containsKey(c) && map.get(c) >= left) {
+                left = map.get(c) + 1;
+            }
+            map.put(c, right);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+}
